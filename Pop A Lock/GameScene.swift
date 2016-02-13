33e4799f5c89177fdb72b37e66b104165c5d9b52
alxcancado <alxcancado@gmail.com>
2016-02-13 17:42:29 +1300

    Bug fix needle

--- Pop A Lock/GameScene.swift
+++ Pop A Lock/GameScene.swift
@@ -2,8 +2,8 @@
 //  GameScene.swift
 //  Pop A Lock
 //
-//  Created by Alex Lima Lopes Cancado on 13/02/16.
-//  Copyright (c) 2016 Alex Lima Lopes Cancado. All rights reserved.
+//  Created by J. Ruof, Meruca on 24.09.15.
+//  Copyright (c) 2015 RuMe Academy. All rights reserved.
 //
 
 import SpriteKit
@@ -14,14 +14,16 @@ class GameScene: SKScene {
     var needle = SKShapeNode()
     var dot = SKShapeNode()
     
-    
     var path = UIBezierPath()
     
     let zeroAngle: CGFloat = 0.0
     
+    var clockwise = Bool()
+    var continueMode = Bool()
+    var maxLevel = NSUserDefaults.standardUserDefaults().integerForKey("maxLevel")
+    
     var started = false
     var touches = false
-    var clockwise = Bool()
     
     var level = 1
     var dots = 0
@@ -29,27 +31,31 @@ class GameScene: SKScene {
     var levelLabel = SKLabelNode()
     var currentScoreLabel = SKLabelNode()
     
-    
     override func didMoveToView(view: SKView) {
+        if continueMode {
+            level = maxLevel
+        }
         layoutGame()
     }
     
     func layoutGame() {
-        backgroundColor = SKColor(red: 26.0/255.0, green:188.0/255.0, blue: 156.0/255.0,  alpha: 1.0)
+        backgroundColor = SKColor(red: 26.0/255.0, green: 188.0/255.0, blue: 156.0/255.0, alpha: 1.0)
         
-        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(M_PI * 2), clockwise: true)
+        if level > maxLevel {
+            NSUserDefaults.standardUserDefaults().setInteger(level, forKey: "maxLevel")
+        }
         
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(M_PI * 2), clockwise: true)
         lock = SKShapeNode(path: path.CGPath)
         lock.strokeColor = SKColor.grayColor()
         lock.lineWidth = 40.0
         self.addChild(lock)
         
-        
         needle = SKShapeNode(rectOfSize: CGSize(width: 40.0 - 7.0, height: 7.0), cornerRadius: 3.5)
         needle.fillColor = SKColor.whiteColor()
-        needle.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 120)
-        needle.zRotation = 3.14 / 2 //90 degrees
-        needle.zPosition = 2.0 //top of the circle
+        needle.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 120.0)
+        needle.zRotation = 3.14 / 2
+        needle.zPosition = 2.0
         self.addChild(needle)
         
         levelLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
@@ -66,6 +72,7 @@ class GameScene: SKScene {
         self.addChild(currentScoreLabel)
         
         newDot()
+        userInteractionEnabled = true
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -81,44 +88,39 @@ class GameScene: SKScene {
     }
     
     func runClockwise() {
-        let dx = needle.position.x - self.frame.width/2
-        let dy = needle.position.y - self.frame.height/2
+        let dx = needle.position.x - self.frame.width / 2
+        let dy = needle.position.y - self.frame.height / 2
         
-        let radian = atan2(dx, dy)
+        let radian = atan2(dy, dx)
         
-        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: radian, endAngle: radian + CGFloat(M_PI * 2), clockwise: true)
-        
-        let run = SKAction.followPath(path.CGPath, asOffset: false, orientToPath: true, duration: 5)
-        
-        needle.runAction(SKAction.repeatActionForever(run).reversedAction()) // .reverseaction just to run to another way
-    }
-
-    func runCounterClockwise() {
-        let dx = needle.position.x - self.frame.width/2
-        let dy = needle.position.y - self.frame.height/2
-        
-        let radian = atan2(dx, dy)
-        
-        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: radian, endAngle: radian + CGFloat(M_PI * 2), clockwise: true)
-        
-        let run = SKAction.followPath(path.CGPath, asOffset: false, orientToPath: true, duration: 5)
-        
-        needle.runAction(SKAction.repeatActionForever(run)) // .reverseaction just to run to another way
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius: 120, startAngle: radian, endAngle: radian + CGFloat(M_PI * 2), clockwise: true)
+        let run = SKAction.followPath(path.CGPath, asOffset: false, orientToPath: true, speed: 200)
+        needle.runAction(SKAction.repeatActionForever(run).reversedAction())
     }
     
-    func dotTouched(){
+    func runCounterClockwise() {
+        let dx = needle.position.x - self.frame.width / 2
+        let dy = needle.position.y - self.frame.height / 2
+        
+        let radian = atan2(dy, dx)
+        
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius: 120, startAngle: radian, endAngle: radian + CGFloat(M_PI * 2), clockwise: true)
+        let run = SKAction.followPath(path.CGPath, asOffset: false, orientToPath: true, speed: 200)
+        needle.runAction(SKAction.repeatActionForever(run))
+    }
+    
+    func dotTouched() {
         if touches == true {
             touches = false
             dots++
             updateLabel()
-            if dots >= level{
+            if dots >= level {
                 started = false
                 completed()
                 return
             }
             dot.removeFromParent()
             newDot()
-            
             if clockwise {
                 runCounterClockwise()
                 clockwise = false
@@ -132,96 +134,84 @@ class GameScene: SKScene {
             gameOver()
         }
     }
-
     
-    func newDot(){
+    func updateLabel() {
+        currentScoreLabel.text = "\(level - dots)"
+    }
+    
+    func newDot() {
         dot = SKShapeNode(circleOfRadius: 15.0)
         dot.fillColor = SKColor(red: 31.0/255.0, green: 150.0/255.0, blue: 255.0/255.0, alpha: 1.0)
         dot.strokeColor = SKColor.clearColor()
         
-        let dx = needle.position.x - self.frame.width/2
-        let dy = needle.position.y - self.frame.height/2
+        let dx = needle.position.x - self.frame.width / 2
+        let dy = needle.position.y - self.frame.height / 2
         
-        let radian = atan2(dx, dy)
+        let radian = atan2(dy, dx)
         
         if clockwise {
-            let tempAngle = CGFloat.random(radian + 1.0, max: radian - 2.5)
-            let tempPath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: tempAngle, endAngle: tempAngle + CGFloat(M_PI * 2), clockwise: true)
-            
+            let tempAngle = CGFloat.random(radian + 1.0, max: radian + 2.5)
+            let tempPath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius: 120, startAngle: tempAngle, endAngle: tempAngle + CGFloat(M_PI * 2), clockwise: true)
             dot.position = tempPath.currentPoint
-
         } else {
             let tempAngle = CGFloat.random(radian - 1.0, max: radian - 2.5)
-            let tempPath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: tempAngle, endAngle: tempAngle + CGFloat(M_PI * 2), clockwise: true)
-            
+            let tempPath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius: 120, startAngle: tempAngle, endAngle: tempAngle + CGFloat(M_PI * 2), clockwise: true)
             dot.position = tempPath.currentPoint
-
         }
         
         self.addChild(dot)
     }
     
-    func completed(){
+    func completed() {
+        userInteractionEnabled = false
         needle.removeFromParent()
-                currentScoreLabel.text = "Won!"
-        
+        currentScoreLabel.text = "Won!"
         let actionRed = SKAction.colorizeWithColor(UIColor(red: 46.0/255.0, green: 204.0/255.0, blue: 113.0/255.0, alpha: 1.0), colorBlendFactor: 1.0, duration: 0.25)
         let actionBack = SKAction.waitForDuration(0.5)
-        
-        self.scene?.runAction(SKAction.sequence([actionRed, actionBack]), completion: { () -> Void in
+        self.scene?.runAction(SKAction.sequence([actionRed,actionBack]), completion: { () -> Void in
             self.removeAllChildren()
             self.clockwise = false
             self.dots = 0
             self.level++
-            self.layoutGame() // start a new game
-            
+            self.layoutGame()
         })
+        
+        
+        
     }
-
     
-    func gameOver(){
+    func gameOver() {
+        userInteractionEnabled = false
         needle.removeFromParent()
         currentScoreLabel.text = "Fail!"
-        
         let actionRed = SKAction.colorizeWithColor(UIColor(red: 149.0/255.0, green: 165.0/255.0, blue: 166.0/255.0, alpha: 1.0), colorBlendFactor: 1.0, duration: 0.25)
         let actionBack = SKAction.waitForDuration(0.5)
-        
-        self.scene?.runAction(SKAction.sequence([actionRed, actionBack]), completion: { () -> Void in
+        self.scene?.runAction(SKAction.sequence([actionRed,actionBack]), completion: { () -> Void in
             self.removeAllChildren()
             self.clockwise = false
             self.dots = 0
-            self.layoutGame() // start a new game
-            
+            self.layoutGame()
         })
+        
+        
+        
     }
     
-    func updateLabel(){
-        currentScoreLabel.text = "\(level - dots)"
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         if started {
-            if needle.intersectsNode(dot){
+            if needle.intersectsNode(dot) {
                 touches = true
             } else {
                 if touches == true {
-                    if !needle.intersectsNode(dot){
+                    if !needle.intersectsNode(dot) {
                         started = false
                         touches = false
                         gameOver()
                     }
-                    
                 }
             }
-        
         }
-        
-        
-        
-        
-        
-        
         
     }
 }
