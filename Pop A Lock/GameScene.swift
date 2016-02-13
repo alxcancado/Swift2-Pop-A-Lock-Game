ecc7930a8e53be6ed6832d7836c82cfeda1784f2
alxcancado <alxcancado@gmail.com>
2016-02-13 15:58:38 +1300

    Creating the circle

--- Pop A Lock/GameScene.swift
+++ Pop A Lock/GameScene.swift
@@ -9,8 +9,25 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var lock = SKShapeNode()
+    var path = UIBezierPath()
+    
+    let zeroAngle: CGFloat = 0.0
+    
     override func didMoveToView(view: SKView) {
+        layoutGame()
+    }
+    
+    func layoutGame() {
+        backgroundColor = SKColor.whiteColor()
         
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(M_PI * 2), clockwise: true)
+        
+        lock = SKShapeNode(path: path.CGPath)
+        lock.strokeColor = SKColor.grayColor()
+        lock.lineWidth = 40.0
+        self.addChild(lock)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
