0c63a2bdd22a8f99aba4291fbb4dbc840b5f1a4e
alxcancado <alxcancado@gmail.com>
2016-02-13 15:44:40 +1300

    Initial commit, spritekit game template

---
+++ Pop A Lock/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Pop A Lock
+//
+//  Created by Alex Lima Lopes Cancado on 13/02/16.
+//  Copyright (c) 2016 Alex Lima Lopes Cancado. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
